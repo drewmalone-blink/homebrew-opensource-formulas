3b0d17bc7839306e5b8a1b61804c5d79d37369d6
Roberto Hidalgo <rob@blinkhealth.com>
2019-12-20 17:40:20 -0600

    Bump go-config-yourself to v1.0.3

--- go-config-yourself.rb
+++ go-config-yourself.rb
@@ -5,9 +5,9 @@ class GoConfigYourself < Formula
   desc 'Want to store secrets in your repo? go config yourself!'
   homepage 'https://github.com/blinkhealth/go-config-yourself'
   stable do
-    url 'https://github.com/blinkhealth/go-config-yourself/releases/download/v1.0.2/gcy-macos-amd64.tgz'
-    sha256 '3ed8bcd803c384a588193306541b523bd2df3db711a46e451ea7a23e8d5a1aa2'
-    version '1.0.2'
+    url 'https://github.com/blinkhealth/go-config-yourself/releases/download/v1.0.3/gcy-macos-amd64.tgz'
+    sha256 '5fa6e368c9a95e32f904d0e458d85e323e8175b77429e08358e95cb6dd8dd05b'
+    version '1.0.3'
   end
   head 'https://github.com/blinkhealth/go-config-yourself', using: GithubPreReleaseDownloadStrategy
 
